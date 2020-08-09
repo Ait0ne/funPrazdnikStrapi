6f6624d6dcf7492ea20b38fadda94b4b7e96b84a
Ait0ne <bonafide112358@gmail.com>
2020-08-09 17:55:23 +0300

    v1.0

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bfc57715383cdf30d53430a14d96ce66'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd2d85bdb-9a15-41df-a673-75e655b43198'
+};
